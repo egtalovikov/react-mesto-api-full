09386f704644578cb390e4d9a1d3df9d397a7a6e
Egor Talovikov <egtalovikov@gmail.com>
2023-02-04 15:25:40 +0500

    feat: добавил заголовок 'Access-Control-Allow-Headers' для CORS

--- backend/app.js
+++ backend/app.js
@@ -46,6 +46,7 @@ app.use((req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
@@ -53,6 +54,10 @@ app.use((req, res, next) => {
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
 
