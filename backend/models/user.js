df431ea7d0ef0777cea6e1bf75972a3ccc96ca1a
Egor Talovikov <egtalovikov@gmail.com>
2023-02-09 20:13:42 +0500

    hotfix: убрал required у полей с default

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,14 +4,12 @@ const { default: isEmail } = require('validator/lib/isEmail');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Исследователь',
@@ -22,7 +20,6 @@ const userSchema = new mongoose.Schema({
       validator: (v) => /(:?(?:https?:\/\/)?(?:www\.)?)?[-a-z0-9]+\.\w/g.test(v),
       message: 'Некорректный URL',
     },
-    required: true,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
