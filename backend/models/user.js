8c91061cbb81b2363219414e31cef0ef1fc10ece
Egor Talovikov <egtalovikov@gmail.com>
2023-02-10 18:06:00 +0500

    hotfix: заменил regex константу на выражение

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const { default: isEmail } = require('validator/lib/isEmail');
-const urlRegex = require('../app');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -18,7 +17,7 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     validate: {
-      validator: (v) => urlRegex.test(v),
+      validator: (v) => /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/.test(v),
       message: 'Некорректный URL',
     },
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
