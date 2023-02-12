f0cb96b1e05d4984085227c3e2c1adf1e33a8c5e
Egor Talovikov <egtalovikov@gmail.com>
2023-02-12 17:31:13 +0500

    hotfix: убрал статус код у ответа

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -14,7 +14,7 @@ const createCard = (req, res, next) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.status(201).send(card))
+    .then((card) => res.send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new ValidationError('Переданы некорректные данные при создании карточки'));
