b7f5a94d863a97c71edecd0f990c697cf9aa4656
Egor Talovikov <egtalovikov@gmail.com>
2023-02-12 19:01:29 +0500

    feat: вынес авторизацию в отдельный файл

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
-const { celebrate, Joi, errors } = require('celebrate');
-
-const { login, createUser } = require('./controllers/users');
+const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const auth = require('./middlewares/auth');
 const handleErrors = require('./middlewares/errors');
@@ -73,26 +71,7 @@ app.get('/crash-test', () => {
   }, 0);
 });
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
-}), login);
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri({
-      scheme: [
-        'http',
-        'https',
-      ],
-    }),
-    about: Joi.string().min(2).max(30),
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
-}), createUser);
+app.use('/', require('./routes/auth'));
 
 app.use(auth);
 
