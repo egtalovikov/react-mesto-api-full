2aca641e658a3f1c9406226e48ee84daf096c739
Egor Talovikov <egtalovikov@gmail.com>
2023-02-12 17:20:38 +0500

    hotfix: убрал статус код при создании карточки

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -5,7 +5,7 @@ const ForbiddenError = require('../errors/forbidden-err');
 
 const getCards = (req, res, next) => {
   Card.find({})
-    .populate('owner')
+    .populate('owner likes')
     .then((cards) => res.send(cards))
     .catch(next);
 };
@@ -14,7 +14,7 @@ const createCard = (req, res, next) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.status(201).send(card))
+    .then((card) => res.send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new ValidationError('Переданы некорректные данные при создании карточки'));
